4220abf9a42f88b87bd3a0da3ad41246b035f709
Lie Aditya Bryan <bryl@in.tum.de>
2024-06-29 19:13:04 +0200

    a

--- includes/cache_module_includes.hpp
+++ includes/cache_module_includes.hpp
@@ -6,6 +6,7 @@
 #include "../includes/lru_cache.hpp"
 #include "../includes/main_memory.hpp"
 #include "../includes/structs.hpp"
+#include "../includes/main_memory_global.hpp"
 
 #include "../includes/cache_address.hpp"
 #include "../includes/cache_config.hpp"
