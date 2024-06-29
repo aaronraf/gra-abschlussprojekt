42b62bc71fe4f74a72f38a59c31b6fb34284ecce
Lie Aditya Bryan <bryl@in.tum.de>
2024-06-29 19:27:52 +0200

    a

--- includes/cache_module_includes.hpp
+++ includes/cache_module_includes.hpp
@@ -1,15 +1,15 @@
 #ifndef CACHE_MODULE_INCLUDES_HPP
 #define CACHE_MODULE_INCLUDES_HPP
 
-#include "../includes/direct_mapped_cache.hpp"
-#include "../includes/four_way_lru_cache.hpp"
-#include "../includes/lru_cache.hpp"
-#include "../includes/main_memory.hpp"
-#include "../includes/structs.hpp"
-#include "../includes/main_memory_global.hpp"
+#include "direct_mapped_cache.hpp"
+#include "four_way_lru_cache.hpp"
+#include "lru_cache.hpp"
+#include "main_memory.hpp"
+#include "structs.hpp"
+#include "main_memory_global.hpp"
 
-#include "../includes/cache_address.hpp"
-#include "../includes/cache_config.hpp"
-#include "../includes/cache_template.hpp"
+#include "cache_address.hpp"
+#include "cache_config.hpp"
+#include "cache_template.hpp"
 
 #endif
---
+++ src/test.cpp
@@ -0,0 +1,7 @@
+#include "../includes/cache_module.hpp"
+#include <systemc>
+
+int sc_main(int argc, char *argv[]) {
+    sc_start();
+    return 0;
+}
