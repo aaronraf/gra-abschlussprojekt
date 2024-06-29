45dc79a1060f884ff5847f30721ec343c8a73c2e
Lie Aditya Bryan <bryl@in.tum.de>
2024-06-29 21:29:16 +0200

    direct mapped fix

--- includes/direct_mapped_cache.hpp
+++ includes/direct_mapped_cache.hpp
@@ -16,11 +16,13 @@ struct CacheEntry {
 class DirectMappedCache : public Cache {
 private:
     // TODO: change size to NoOfCacheLine
-    CacheEntry cache_entry[100];
+    CacheEntry* cache_entry[100];
 
-    void replace(int address, CacheEntry current_entry, int number_of_offset);
+    void replace(int address, CacheEntry* current_entry, int number_of_offset);
 
 public:
+    DirectMappedCache();
+
     int read_from_cache(int address, CacheConfig cache_config) override;
 
     void write_to_cache(int address, CacheConfig cache_config, int data_to_write) override;
--- src/direct_mapped_cache.cpp
+++ src/direct_mapped_cache.cpp
@@ -1,37 +1,47 @@
 #include "../includes/direct_mapped_cache.hpp"
 #include "../includes/main_memory_global.hpp"
 
-void DirectMappedCache::replace(int address, CacheEntry current_entry, int number_of_offset) {
+DirectMappedCache::DirectMappedCache() {
+    for (int i = 0; i < 100; ++i) {
+        cache_entry[i] = new CacheEntry(); // Allocate new CacheEntry object
+        cache_entry[i]->tag = 0; // Initialize tag to 0
+        for (int j = 0; j < 100; ++j) {
+            cache_entry[i]->data[j] = 0; // Initialize data to 0
+        }
+    }
+}
+
+void DirectMappedCache::replace(int address, CacheEntry* current_entry, int number_of_offset) {
     int start_address_to_fetch = (address / number_of_offset) * number_of_offset;
     int last_address_to_fetch = start_address_to_fetch + number_of_offset - 1;
 
     for (int ram_address = start_address_to_fetch, offset = 0; ram_address <= last_address_to_fetch; ram_address++, offset++) { // O(1)
-        current_entry.data[offset] = main_memory->read_from_ram(ram_address);
+        current_entry->data[offset] = main_memory->read_from_ram(ram_address);
     }
 }
 
 int DirectMappedCache::read_from_cache(int address, CacheConfig cache_config) {
     CacheAddress cache_address(address, cache_config);
-    CacheEntry current_entry = cache_entry[cache_address.index];
+    CacheEntry* current_entry = cache_entry[cache_address.index];
     
-    if (current_entry.tag != cache_address.tag) {
+    if (current_entry->tag != cache_address.tag) {
         replace(address, current_entry, cache_config.number_of_offset);
-        current_entry.tag = cache_address.tag;
+        current_entry->tag = cache_address.tag;
     }
     
-    return current_entry.data[cache_address.offset];
+    return current_entry->data[cache_address.offset];
 }
 
 void DirectMappedCache::write_to_cache(int address, CacheConfig cache_config, int data_to_write) {
     CacheAddress cache_address(address, cache_config);
-    CacheEntry current_entry = cache_entry[cache_address.index];
+    CacheEntry* current_entry = cache_entry[cache_address.index];
     
-    if (current_entry.tag != cache_address.tag) {
+    if (current_entry->tag != cache_address.tag) {
         replace(address, current_entry, cache_config.number_of_offset);
-        current_entry.tag = cache_address.tag;
+        current_entry->tag = cache_address.tag;
     }
     
-    current_entry.data[cache_address.offset] = data_to_write;
+    current_entry->data[cache_address.offset] = data_to_write;
     main_memory->write_to_ram(address, data_to_write);
 }
 
